52124a49be0c3ec50aa988de0562b185e0185d68
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2010-12-05 02:41:02 +0100

    Fix silly mistakes. (krohm stupid shell syntax..)

--- update-out-of-date.sh
+++ update-out-of-date.sh
@@ -7,9 +7,9 @@ OUTDATED=`/usr/ports/infrastructure/build/./out-of-date |awk '{print $1}'`
 for pkg in ${OUTDATED}; do
 	echo "Do you want to update [y/n]: ${pkg}"
 	read CHOICE
-	if [$CHOICE = 'y' ]; then
+	if [ $CHOICE = 'y' ]; then
 		echo "Will do make update for ${pkg}"
-		make update ${pkg}
+		cd /usr/ports/${pkg} && sudo make update
 	else
 		continue
 	fi
