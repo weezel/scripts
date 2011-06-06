e209d58684c8fa27c7d6b360a88c27d363256b7b
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2011-06-06 20:01:38 +0200

    Update netchoose.sh to correspond obsd 4.9 syntax. Actually this have been around for awhile but I forgot to push it.

--- netchoose.sh
+++ netchoose.sh
@@ -1,30 +1,37 @@
 #!/bin/sh
 
+CHOICE=3 # Default to Wireless
 WIRELESS_DEV=`ifconfig -a|grep -B 3 wlan|head -1|awk '{print $1}'|sed s/\://`
+KNOWN_NETWORKS="{"wlaku"; "utapac"; "Wireless"; }"
 
-echo "Formatting ${WIRELESS_DEV}.."
+echo "Reset wireless device ${WIRELESS_DEV}.."
 ifconfig ${WIRELESS_DEV} -bssid -chan media autoselect nwid -nwkey -wpa -wpapsk
 
-echo "\nChoose a network: "
-
-echo "1) wlaku"
-echo "2) utapac"
-echo "3) Wireless"
-echo "0) Join existing WPA/WPA2 network.."
-echo "q) quit"
-
-echo "choice > "
-read CHOICE
+case $1 in
+	1) CHOICE=1;;
+	2) CHOICE=2;;
+	3) CHOICE=3;;
+	0) CHOICE=0;;
+	*) echo "\nChoose a network: "
+	echo "1) wlaku"
+	echo "2) utapac"
+	echo "3) Wireless"
+	echo "0) Join existing WPA/WPA2 network.."
+	echo "q) quit"
+	echo ""
+	echo "choice > "
+	read CHOICE ;;
+esac
 
 case $CHOICE in
     1) echo "Trying to join wlaku.." &&
-       ifconfig ${WIRELESS_DEV} nwid wlaku wpa wpapsk `wpa-psk wlaku pass` ;;
+       ifconfig ${WIRELESS_DEV} nwid wlaku wpakey "secret" ;;
     2) echo "Trying to join utapac.." &&
        ifconfig ${WIRELESS_DEV} nwid utapac ;;
     3) echo "Trying to join Wireless.."
-       ifconfig ${WIRELESS_DEV} nwid Wireless wpa wpakey "pass";;
+       ifconfig ${WIRELESS_DEV} nwid Wireless wpa wpakey "secret" ;;
     0) echo "Scanning networks.." &&
-       ifconfig iwn0 scan |grep nwid|less &&
+       ifconfig ${WIRELESS_DEV} scan |grep nwid|less &&
        echo "Name of the network > " &&
        read CHOICE &&
        echo "Password > "
@@ -36,6 +43,6 @@ case $CHOICE in
        exit 1 ;;
 esac
 
-echo "Trying to get a lease from the dhcpd.."
+echo "Trying to get lease from the dhcpd.."
 dhclient ${WIRELESS_DEV}
 
