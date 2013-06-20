1276f7a2fb0e4ac762ee89126da1374be779b5d8
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2013-06-21 01:47:45 +0300

    external_disp.sh is more usable for my needs now

--- external_disp.sh
+++ external_disp.sh
@@ -1,57 +1,46 @@
 #!/bin/sh
 
-ORIENTATION=""
-#PRIMARY_DISP="LVDS1"
-#EXTERNAL_DISP="VGA1"
+PRIMARY_DISP="LVDS1"
+EXTERNAL_DISP=$(xrandr -q |grep " connected" |grep -v ${PRIMARY_DISP} | awk '{print $1}')
+set -A DISP_OPTIONS '(d)ouble_monitors' '(m)onitor' '(p)rojector' '(t)elevision' '(r)eset'
 
-echo "Connected displays:"
-xrandr|grep -E "\bconnected\b"|awk '{print $1}'
+printf "Connected ports: %s, %s\n\n" "${PRIMARY_DISP}" "${EXTERNAL_DISP}"
 
-case $2 in
-o)
-	xrandr --output $1 --off
-	exit
+echo "Which external display to use?"
+for o in ${DISP_OPTIONS[@]}; do
+	echo ${o}
+done
+
+printf "> "
+read CHOICE
+
+case ${CHOICE} in
+d)
+	xrandr --output ${PRIMARY_DISP} --auto \
+		--output ${EXTERNAL_DISP} --auto --right-of ${PRIMARY_DISP}
 ;;
-esac
-
-case $3 in
-l)
-	ORIENTATION="--left-of"
+m)
+	xrandr --output ${PRIMARY_DISP} --off \
+		--output ${EXTERNAL_DISP} --auto
+;;
+p)
+	xrandr --output ${PRIMARY_DISP} --auto \
+		--output ${EXTERNAL_DISP} --mode 1280x720 --rate 60.0
+		#--set audio force-dvi --above ${PRIMARY_DISP}
+;;
+t)
+	xrandr --output ${PRIMARY_DISP} --auto \
+		--output ${EXTERNAL_DISP} --mode 1280x720 --rate 60.0 \
+		--set audio force-dvi --above ${PRIMARY_DISP}
 ;;
 r)
-
-	ORIENTATION="--right-of"
-;;
-a)
-
-	ORIENTATION="--above"
-;;
-b)
-
-	ORIENTATION="--below"
-;;
-s)
-
-	ORIENTATION="--same-as"
+	xrandr --output ${PRIMARY_DISP} --auto \
+		--output ${EXTERNAL_DISP} --off
 ;;
 *)
-	echo ""
-	echo "Usage:"
-	echo "external_disp.sh PRIMARY_DISP EXTERNAL_DISP ORIENTATION"
-	echo ""
-	echo "Or to switch off external monitor: "
-	echo "external_disp.sh EXTERNAL_DISPLAY o"
-	echo ""
-	echo "Display orientation"
-	echo "(l)eft"
-	echo "(r)ight"
-	echo "(a)bove"
-	echo "(b)elow"
-	exit
+	echo "'${CHOICE}' is not an option."
+	return 1
 ;;
 esac
 
-# finally set the screens
-#xrandr --output $PRIMARY_DISP --primary --auto --output $EXTERNAL_DISP --auto --output $EXTERNAL_DISP $ORIENTATION $PRIMARY_DISP
-xrandr --output $1 --auto --output $2 --primary --auto --output $2 $ORIENTATION $1
-
+return 0
