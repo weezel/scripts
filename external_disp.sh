eaa31bd134a876ea880e8b0e39f80bcaada0abf4
Ville Valkonen <weezelding@gmail.com>
2015-11-28 18:29:02 +0200

    Better redability and functionality.
    Width is max 80 chars.
    
    Instead of handling different possible monitors, handle certain monitor
    modes instead. Designed to be used with one monitor only, though
    possible to extend pretty easily.

--- external_disp.sh
+++ external_disp.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 PRIMARY_DISP="eDP1"
-EXTERNAL_DISP=$(xrandr -q |awk '$2 ~ "^connected$" {print $1}' |grep -v "${PRIMARY_DISP}")
-set -A DISP_OPTIONS '(d)ouble_monitors' '(m)onitor' '(p)rojector' '(t)elevision' '(r)eset'
+EXTERNAL_DISP=$(xrandr -q |awk '$2 ~ "^connected$" {print $1}' | \
+		grep -v "${PRIMARY_DISP}")
+set -A DISP_OPTIONS '(m)irrored' '(p)rimary_monitor_only' \
+		    '(s)triped_mode' '(r)eset'
 
-printf "Connected ports: %s, %s\n\n" "${PRIMARY_DISP}" "${EXTERNAL_DISP}"
+printf "PRIMARY display : %8s\n" "${PRIMARY_DISP}"
+printf "EXTERNAL display: %8s\n\n" "${EXTERNAL_DISP}"
 
-echo "Which external display to use?"
+echo "Which display mode to use?"
 for o in ${DISP_OPTIONS[@]}; do
 	echo ${o}
 done
@@ -15,23 +18,17 @@ printf "> "
 read CHOICE
 
 case ${CHOICE} in
-d)
-	xrandr --output ${PRIMARY_DISP} --auto \
-		--output ${EXTERNAL_DISP} --auto --right-of ${PRIMARY_DISP}
-;;
 m)
+	xrandr --output ${PRIMARY_DISP} --auto \
+		--output ${EXTERNAL_DISP} --auto --same-as "${PRIMARY_DISP}"
+;;
+p)
 	xrandr --output ${PRIMARY_DISP} --off \
 		--output ${EXTERNAL_DISP} --auto
 ;;
-p)
+s)
 	xrandr --output ${PRIMARY_DISP} --auto \
-		--output ${EXTERNAL_DISP} --mode 1280x720 --rate 60.0
-		#--set audio force-dvi --above ${PRIMARY_DISP}
-;;
-t)
-	xrandr --output ${PRIMARY_DISP} --auto \
-		--output ${EXTERNAL_DISP} --mode 1280x720 --rate 60.0 \
-		--set audio force-dvi --above ${PRIMARY_DISP}
+		--output ${EXTERNAL_DISP} --auto --right-of "${PRIMARY_DISP}"
 ;;
 r)
 	xrandr --output ${PRIMARY_DISP} --auto \
