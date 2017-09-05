436293e84c60e90eef2f577b2cf024c816915d93
Ville Valkonen <weezelding@gmail.com>
2017-09-05 21:40:38 +0300

    Monitor configuration utility. Handy to have it binded in i3's configuration.

---
+++ monitor_config.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# $ cat ~/.screenconfig
+# _laptop="eDP1"
+# _primary="DP2-2"
+# _secondary="DP2-1"
+
+. ~/.screenconfig
+
+_monitorcount=$(xrandr |grep -c " connected")
+case "${_monitorcount}" in
+2)
+	xrandr --output ${_laptop} --off \
+		--output ${_primary} --auto --primary
+	;;
+3)
+	xrandr --output ${_laptop} --off \
+		--output ${_primary} --auto --primary \
+		--output ${_secondary} --auto --rotate right \
+		--right-of ${_primary}
+	;;
+esac
+
