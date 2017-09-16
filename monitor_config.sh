20e4110fdab1d18d45c56510d20daa788298599e
Ville Valkonen <weezelding@gmail.com>
2017-09-16 09:42:53 +0300

    monitor_config.sh: with one monitor connected, we want to use that.

--- monitor_config.sh
+++ monitor_config.sh
@@ -9,6 +9,9 @@
 
 _monitorcount=$(xrandr |grep -c " connected")
 case "${_monitorcount}" in
+1)
+	xrandr --output ${_laptop} --on --auto --primary
+
 2)
 	xrandr --output ${_laptop} --off \
 		--output ${_primary} --auto --primary
