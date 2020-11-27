7a49d7e603df28e5c06cb5829f9b817a9e97fbef
Ville Valkonen <weezelding@gmail.com>
2020-11-27 23:03:04 +0200

    A lousy solution for automatic screen scaling and handling (un)docking events.

--- monitor_config.sh
+++ monitor_config.sh
@@ -16,7 +16,21 @@ case "${_monitorcount}" in
 	;;
 2)
 	xrandr --output ${_laptop} --off \
-		--output ${_primary} --auto --primary
+		--output ${_primary} --dpi 120 --auto --primary
+	xinput --set-prop "Synaptics TM3075-002" "Device Accel Profile" 1
+	xset mouse 100 5
+	synclient HorizTwoFingerScroll=1
+	synclient HorizScrollDelta=15
+	synclient PalmDetect=1
+	synclient PalmMinWidth=15
+	synclient PalmMinZ=30
+	synclient TapButton1=0
+	synclient TapButton2=0
+	synclient TapButton3=0
+	# Keyboard related
+	xset b off
+	xset r rate 300 30
+	setxkbmap -layout us,fi -option grp:caps_toggle
 	;;
 3)
 	xrandr --output ${_laptop} --off \
