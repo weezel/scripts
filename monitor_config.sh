82558c8c7e47ce603653f49fa16241a0905cb97a
Ville Valkonen <weezelding@gmail.com>
2021-12-09 20:04:00 +0200

    Remove useless cruft and make it more simple.

--- monitor_config.sh
+++ monitor_config.sh
@@ -2,41 +2,17 @@
 
 set -euxo pipefail
 
-# $ cat ~/.screenconfig
-# _laptop="eDP1"
-# _primary="DP2-2"
-# _secondary="DP2-1"
+# Read monitor related configurations from the file.
+# This makes it possible to run certain commands when
+# certain screen setup is attached.
 
-. ~/.screenconfig
-
-_monitorcount=$(xrandr |grep -c " connected")
-case "${_monitorcount}" in
+readonly monitorcount=$(xrandr |grep -c " connected")
+case "${monitorcount}" in
 1)
-	xrandr --output ${_laptop} --dpi 120 --auto --primary
+	. "${HOME}/.screenconfig_1"
 	;;
 2)
-	xrandr --output ${_laptop} --off \
-		--output ${_primary} --dpi 120 --auto --primary
-	xinput --set-prop "Synaptics TM3075-002" "Device Accel Profile" 1
-	xset mouse 100 5
-	synclient HorizTwoFingerScroll=1
-	synclient HorizScrollDelta=15
-	synclient PalmDetect=1
-	synclient PalmMinWidth=15
-	synclient PalmMinZ=30
-	synclient TapButton1=0
-	synclient TapButton2=0
-	synclient TapButton3=0
-	# Keyboard related
-	xset b off
-	xset r rate 300 30
-	setxkbmap -layout us,fi -option grp:caps_toggle
-	;;
-3)
-	xrandr --output ${_laptop} --off \
-		--output ${_primary} --auto --primary \
-		--output ${_secondary} --auto --rotate right \
-		--right-of ${_primary}
+	. "${HOME}/.screenconfig_2"
 	;;
 esac
 
